0a6392a7f0b7626629da81852cb96aa18f93707e
Jorje <lursmanashviligiorgi88@gmail.com>
2025-04-25 15:50:16 +0400

    added utility functions

---
+++ backend/src/utils/database.ts
@@ -0,0 +1,39 @@
+import { Database } from "better-sqlite3";
+import { FlashcardRow, PracticeRecordRow } from "../types";
+/**
+ * Creates the flashcards and practicerecords tables in the given database if
+ * they do not already exist.
+ * @param {Database} db - The database to create the tables in
+ */
+export function createTables(db: Database) {
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS flashcards (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        front TEXT NOT NULL,
+        back TEXT NOT NULL,
+        hint TEXT,
+        tags TEXT,
+        scheduledDay INTEGER NOT NULL DEFAULT 0
+    );
+    `);
+
+  // Execute a query to create a table
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS practicerecords (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp INTEGER  NOT NULL,
+        difficulty INTEGER  NOT NULL,
+        oldday INTEGER  NOT NULL,
+        newday INTEGER  NOT NULL,
+        FOREIGN KEY (cardId) REFERENCES flashcards(id)
+    );
+    `);
+}
+
+function parseFlashcard(row: FlashcardRow) {
+  //TODO
+}
+
+export function get_flashcards(db: Database, cardId: number): Array<Flashcard> {
+  //TODO
+}
