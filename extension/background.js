70f2987f3f9166d45305508eb36fa8d171c4773b
Dachi Tatunashvili <tatunashvilidachi@gmail.com>
2025-04-27 20:45:56 +0400

    added new file content.js also had to make changes in manifest.json by adding script in permissions array, and sent a message from background.js to content.js , however I have to dig into that just a little bit more

--- extension/background.js
+++ extension/background.js
@@ -4,13 +4,23 @@ chrome.contextMenus.create({
     title: "Add to Flashcards", 
     contexts: ["selection"] 
   });
-
-  chrome.contextMenus.onClicked.addListener((info, tab) => {
-    // if anything in Google chrome was clicked on 
-    // info ინფორმაცია დაკლიკვის შესახებ tab - სად დააკლიკა ადამიანმა იმ ტაბის შესახებ ინფორმაცია
+//async because we use await to asynchronously get the information onto the content.js
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+    
+    // if what we click in contextMenus has an id of addtoflashcard (meaning that we just მოვნიშეთ რაღაც ტექსტი და we right clicked on it and then clicked on add tio flashcard extension)
+    //this will trigger this id   id: "addToFlashcards",  და მერე აქ შემოვა ნახავს რომ ეს აიდი არის გამოყენებული და გადააგზავნის ამ ინფოს content.js ში.
     if (info.menuItemId === "addToFlashcards") {
-        // if you see it clearly, we compare the id of menuItem and that means that if it is what we just created , we send the id of the tab that we did it on
-        // then if there is a click on addtoFlashcard button (i think it has to be called button , but not so sure lol)
-        chrome.tabs.sendMessage(tab.id);
+        //await because we need content.js to be running and loaded as well so we wait for this
+        // this is one of my reasons why I changed the code for background.js
+        // because previously it had some problems sending the text to the content.js , but now he is able to do it.
+        await chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ['content.js']
+        });
+        //now the last step , to just confirm that everything went 'okay'
+        console.log("Flashcard Adder: Content script injected successfully.");
+        
+        await chrome.tabs.sendMessage(tab.id, { action: "get_selected_text" });
+        console.log("Flashcard Adder: Message sent to content script.");
     }
 });
